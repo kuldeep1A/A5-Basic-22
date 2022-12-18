dde2599180c74378dbcfa510e10ce6b9341ca343
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2022-12-18 18:07:20 +0530

    Add files via upload

---
+++ 01_BandName.py
@@ -0,0 +1,4 @@
+print("Welcome to Band name Generator ")
+name = input("What is name of city, where you grew up?\n")
+pet = input("What is name your pet?\n")
+print(f"Your Band name is {name} {pet} ")
