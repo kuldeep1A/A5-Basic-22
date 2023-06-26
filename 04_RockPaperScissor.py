3e8f69e1298e6203536399f680b659d0a9db7d80
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2023-06-27 01:07:51 +0530

    Update 04_RockPaperScissor.py
    THIS IS FOR SLOW

--- 04_RockPaperScissor.py
+++ 04_RockPaperScissor.py
@@ -26,31 +26,6 @@ scissors = '''
 ---.__(___)
 '''
 
-# game_image = [rock, paper, scissors]
-# user_choice = int(
-#     input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-
-# if user_choice >= 3 or user_choice < 0:
-#     print("You typed an invalid number, you lose.")
-# else:
-#     print(game_image[user_choice])
-#     computer_choice = random.randint(0, 2)
-#     # computer_choice = 1
-#     print("Computer Choice:")
-#     print(game_image[computer_choice])
-
-#     if user_choice == 0 and computer_choice == 2:
-#         print("You Win!")
-#     elif computer_choice == 0 and user_choice == 2:
-#         print("You lose")
-#     elif computer_choice > user_choice:
-#         print("You lose")
-#     elif user_choice > computer_choice:
-#         print("You win")
-#     elif computer_choice == user_choice:
-#         print("It's a Draw")
-
-
 game_image = [rock, paper, scissors]
 is_again = True
 while is_again:
